df58eabf75d818f2e1f35b6fd5f696db341fa07b
IzumiSakai1995 <IzumiSakai1995@gmail.com>
2018-07-10 22:59:36 +0800

    pc_01

---
+++ pc_01.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/07/10 22:54:36
+# @Author  : Izumi Sakai
+# @File    : pc_01.py
+# @Software: PyCharm
+
+import requests
+# from bs4 import BeautifulSoup
+# def main():
+#     url = 'http://www.pythonchallenge.com/pc/def/map.html'
+#     soup = BeautifulSoup(url,'lxml')
+#     soup.
+#
+#
+# if __name__ == '__main__':
+#     main()
